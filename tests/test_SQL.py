0cea1da9c8fef85f9ca4baa845d63901e24ece92
Mike Judd <Michael.Judd@Wahoofitness.com>
2022-01-03 11:54:32 -0500

    alot of tests and tidying

--- tests/test_SQL.py
+++ tests/test_SQL.py
@@ -1,7 +1,10 @@
 import mysql.connector as mysql
+import pytest
+
 from algo_config import HOST, DATABASE, USER, PASSWORD
 
 
+@pytest.mark.xfail
 def test_connection():
     db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
     assert db_connection.is_connected()
