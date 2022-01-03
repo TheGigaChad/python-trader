8a280c6194dfd4142c92301e79b9291de4b92de3
Mike Judd <Michael.Judd@Wahoofitness.com>
2022-01-03 12:20:17 -0500

    alot of tests and tidying

--- tests/test_SQL.py
+++ tests/test_SQL.py
@@ -1,10 +1,10 @@
 import mysql.connector as mysql
 import pytest
 
-from algo_config import HOST, DATABASE, USER, PASSWORD
+from algo_config import SQL_SERVER_HOST, SQL_SERVER_DATABASE, SQL_SERVER_USER, SQL_SERVER_PASSWORD
 
 
 @pytest.mark.xfail
 def test_connection():
-    db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+    db_connection = mysql.connect(host=SQL_SERVER_HOST, database=SQL_SERVER_DATABASE, user=SQL_SERVER_USER, password=SQL_SERVER_PASSWORD)
     assert db_connection.is_connected()
--- tests/test_exchanges.py
+++ tests/test_exchanges.py
@@ -1,29 +1,29 @@
 import pytest
 import alpaca_trade_api as tradeapi
-from algo_config import KEY, SECRET
+from algo_config import ALPACA_PAPER_KEY, ALPACA_PAPER_SECRET
 
 
 # ALPACA TESTS
 #@pytest.mark.xfail
 def test_alpaca_paper_account():
-    api = tradeapi.REST(KEY, SECRET, 'https://paper-api.alpaca.markets')
+    api = tradeapi.REST(ALPACA_PAPER_KEY, ALPACA_PAPER_SECRET, 'https://paper-api.alpaca.markets')
     account_number = "PA3Y2VIN22VU"
     assert api.get_account().account_number == account_number
 
 
 #@pytest.mark.xfail
 def test_alpaca_paper_account_blocked():
-    api = tradeapi.REST(KEY, SECRET, 'https://paper-api.alpaca.markets')
+    api = tradeapi.REST(ALPACA_PAPER_KEY, ALPACA_PAPER_SECRET, 'https://paper-api.alpaca.markets')
     assert not api.get_account().account_blocked
 
 
 #@pytest.mark.xfail
 def test_alpaca_paper_trading_blocked():
-    api = tradeapi.REST(KEY, SECRET, 'https://paper-api.alpaca.markets')
+    api = tradeapi.REST(ALPACA_PAPER_KEY, ALPACA_PAPER_SECRET, 'https://paper-api.alpaca.markets')
     assert not api.get_account().trading_blocked
 
 
 #@pytest.mark.xfail
 def test_alpaca_paper_transfers_blocked():
-    api = tradeapi.REST(KEY, SECRET, 'https://paper-api.alpaca.markets')
+    api = tradeapi.REST(ALPACA_PAPER_KEY, ALPACA_PAPER_SECRET, 'https://paper-api.alpaca.markets')
     assert not api.get_account().transfers_blocked
