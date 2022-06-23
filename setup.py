62dec895bce5c13caa1789ba76c11f1a0efe1cc7
Michael <michaeljuddnz@gmail.com>
2022-06-23 17:06:10 +0700

    Working on model Manager and Training manager structure, adjusting setup.py to try test sh files

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('requirements.txt', 'r') as f:
     install_requires = f.read().splitlines()
 
 setuptools.setup(name='pytrader',
-                 version='0.0.1',
+                 version='0.0.2',
                  author='Michael Judd',
                  author_email='MichaelJuddNZ@gmail.com',
                  description='Python Trader',
                  url='https://github.com/TheGigaChad/python-trader',
-                 packages=setuptools.find_packages(),
+                 packages=['pytrader', 'pytrader.common', 'pytrader.models', 'pytrader.ml'],
                  install_requires=install_requires)
