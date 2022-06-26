bd3278723803a6c5f1f6ac47613d204e125a6474
Michael <michaeljuddnz@gmail.com>
2022-06-26 21:27:32 +0700

    V0.08

--- pytrader/ml/mlTrainingManager.py
+++ pytrader/ml/mlTrainingManager.py
@@ -1,8 +1,10 @@
 import os
-from typing import List
+from typing import List, Optional
 
 from pytrader import common, models
 from pytrader.ml.mlTrainingInstance import MLTrainingInstance
+
+Log: common.Log = common.Log(__file__)
 
 
 class MLTrainingManager:
@@ -14,7 +16,7 @@ class MLTrainingManager:
         """
         self.__status: common.State = common.State.INIT
         self.__model_manager: models.ModelManager = models.ModelManager()
-        self.__instances: List[MLTrainingInstance] = self.__get_all_instances()
+        self.__instances: List[MLTrainingInstance] = self.__init_instances()
 
     def monitor_instances(self):
         """
@@ -23,16 +25,58 @@ class MLTrainingManager:
         # TODO
         pass
 
-    def train_all(self):
-        # TODO this is way down the line i think
-        self.__status = common.State.RUNNING
-        self.__status = common.State.READY
-        pass
+    def __get_instance_by_name(self, name: str) -> Optional[MLTrainingInstance]:
+        """
+        Returns the instance if it exists in our list of instances.
+        :param name: name of the training instance we want.
+        :return: the instance, or None if it doesn't exist in our instance list.
+        """
+        for instance in self.__instances:
+            if instance.name == name:
+                return instance
+        return None
 
-    def train_model(self, name: str):
+    def __get_or_create_instance_by_name(self, name: str) -> MLTrainingInstance:
+        """
+        Returns the relevant instance if it exists in the instance list or a model exists in the local stores
+        else it will create a new model.
+        :param name: Name of the asset that the model corresponds to.
+        :return: The corresponding instance.
+        """
+        instance: MLTrainingInstance = self.__get_instance_by_name(name)
+        if instance is not None:
+            return instance
+
+        Log.d(f"No instance exists for {name}, let's create one.")
+        self.__model_manager.update_local_model(name)
+        model: models.ModelPackage = self.__model_manager.get_model_by_name(name)
+        if model is not None:
+            return MLTrainingInstance(name, model)
+
+        Log.d(f"No model exists for {name}, let's create one.")
+        model = self.__model_manager.create_model_package(name)
+        return MLTrainingInstance(name, model)
+
+    def train_all(self):
+        """
+        Tells each instance to begin training.
+        :return: None
+        """
         self.__status = common.State.RUNNING
-        self.__status = common.State.READY
-        pass
+        for instance in self.__instances:
+            instance.train()
+
+    def train(self, asset_name: str):
+        """
+        Trains the specified model.
+        :param asset_name: name of the asset we want to train a model for. 
+        """
+        self.__status = common.State.RUNNING
+        instance: MLTrainingInstance = self.__get_instance_by_name(asset_name)
+        if instance is None:
+            Log.w(f"Cannot locate an instance for {asset_name}, please try again or create one.")
+            return
+        instance.train()
 
     def create_model(self, name: str) -> models.ModelPackage:
         """
@@ -40,19 +84,22 @@ class MLTrainingManager:
         :param name: name of the asset
         :return:a model package containing all relevant data and info.
         """
-        return self.__model_manager.create_new_model_package(name)
+        return self.__model_manager.create_model_package(name)
 
     def create_and_train(self, asset_name: str):
         """
         Creates and trains a model for specified asset.
         :param asset_name: name of asset.
         """
-        model: models.ModelPackage = self.__model_manager.create_new_model_package(asset_name)
-        instance: MLTrainingInstance = MLTrainingInstance(model.name, model)
-        self.__instances.append(instance)
+        instance: MLTrainingInstance = self.__get_or_create_instance_by_name(asset_name)
         instance.train()
 
-    def __get_all_instances(self) -> List[MLTrainingInstance]:
+    def __init_instances(self) -> List[MLTrainingInstance]:
+        """
+        Initialises all training instances.  This will download all models and then populate the instance list
+        with them.
+        :return: All available instances.
+        """
         instances: List[MLTrainingInstance] = []
         self.__model_manager.populate_local_repo()
         for root, dirs, files in os.walk(self.__model_manager.directory):
---
+++ pytrader/tests/test_models.py
@@ -0,0 +1,82 @@
+import os
+from pathlib import Path
+
+import pytest
+from tensorflow.python import keras
+
+from pytrader import models, common
+
+PATH = Path(__file__).parent
+DATA_PATH = PATH / 'data'
+
+
+def test_delete_local_model():
+    model_mgr: models.ModelManager = models.ModelManager()
+    model_mgr.directory = DATA_PATH
+    model_name: str = "test_model.h5"
+    model_mgr.delete_local_model_by_name(model_name)
+    file_path: Path = DATA_PATH / model_name
+    if file_path.is_file():
+        os.remove(file_path)
+    assert not file_path.is_file()
+
+
+@pytest.mark.dependency(depends=["test_delete_local_model"])
+def test_create_local_model():
+    model_name: str = "test_model.h5"
+    file_path: Path = DATA_PATH / model_name
+    model: keras.models.Model = keras.models.Sequential([
+        keras.Input(shape=784)
+    ])
+    model.save(filepath=file_path, overwrite=False)
+    assert file_path.is_file()
+
+
+@pytest.mark.dependency(depends=["test_create_local_model"])
+def test_push_local_model():
+    asset_name: str = "test_model"
+    model_manager: models.ModelManager = models.ModelManager()
+    model_manager.directory = DATA_PATH
+    model_pkg: models.ModelPackage = model_manager.get_model_by_name(asset_name)
+    status: common.GenericStatus = model_manager.upload_cloud_model(model_pkg)
+    assert status == common.GenericStatus.SUCCESSFUL
+
+
+@pytest.mark.dependency(depends=["test_push_local_model"])
+def test_pull_remote_model():
+    model_mgr: models.ModelManager = models.ModelManager()
+    model_mgr.directory = DATA_PATH
+    model_name: str = "test_model"
+    model_filename: str = "test_model.h5"
+    model_mgr.delete_local_model_by_name(model_name)
+    model_path: Path = model_mgr.directory / model_filename
+    assert not model_path.is_file()
+    model_mgr.download_model_by_name(model_name)
+    assert model_path.is_file()
+
+
+@pytest.mark.dependency(depends=["test_pull_remote_model"])
+def test_update_local_model():
+    asset_name: str = "test_model"
+    model_manager: models.ModelManager = models.ModelManager()
+    model_manager.directory = DATA_PATH
+    status: common.GenericStatus = model_manager.update_local_model(asset_name)
+    assert status == common.GenericStatus.SUCCESSFUL
+
+
+@pytest.mark.dependency(depends=["test_update_local_model"])
+def test_populate_dir():
+    model_mgr: models.ModelManager = models.ModelManager()
+    model_mgr.directory = DATA_PATH
+    status: common.GenericStatus = model_mgr.download_all_models()
+    assert status == common.GenericStatus.SUCCESSFUL
+
+
+@pytest.mark.dependency(depends=["test_update_local_model"])
+def test_delete_remote_model():
+    asset_name: str = "test_model"
+    model_manager: models.ModelManager = models.ModelManager()
+    model_manager.directory = DATA_PATH
+    model_pkg: models.ModelPackage = model_manager.get_model_by_name(asset_name)
+    status: common.GenericStatus = model_manager.delete_cloud_model(model_pkg)
+    assert status == common.GenericStatus.SUCCESSFUL
